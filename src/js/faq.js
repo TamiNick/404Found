32ca2dd70cc85209c887d8d6d48fd7146a794edb
Yurii Osinnyi <osinnyiyurii@gmail.com>
2025-11-08 12:18:49 +0200

    Work in progress

---
+++ src/js/faq.js
@@ -0,0 +1,19 @@
+import Accordion from 'accordion-js';
+import '../css/faq.css';
+export function initFaqAccordion() {
+  const accordionContainer = document.querySelector('.accordion-container');
+
+  if (!accordionContainer) {
+    return;
+  }
+
+  try {
+    new Accordion('.accordion-container', {
+      duration: 400,
+      showMultiple: false,
+      collapse: true,
+      onOpen: currentElement => {},
+      onClose: currentElement => {},
+    });
+  } catch (error) {}
+}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import './css/styles.css';
+// import Accordion from 'accordion-js';
+
+// window.Accordion = Accordion;
+// import { initFaqAccordion } from './js/faq.js';
+// document.addEventListener('DOMContentLoaded', () => {
+//   initFaqAccordion();
+// });
